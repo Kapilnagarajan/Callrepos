c286418cd5a8c26b3b587e46085e6b6fe8b822b5
PraveenKR7 <praveenk_r@outlook.com>
2022-03-10 10:56:50 +0530

    Added code to GitThird with kapil

--- src/test/java/com/hexa/IntroGit.java
+++ src/test/java/com/hexa/IntroGit.java
@@ -13,4 +13,24 @@ public class IntroGit {
 	public void call3() {
 
 	}
+
+	public void method1() {
+		System.out.println("1");
+	}
+
+	public void method2() {
+		System.out.println("2");
+	}
+
+	public void method3() {
+		System.out.println("3");
+	}
+
+	public void method4() {
+		System.out.println("4");
+	}
+
+	public void method5() {
+		System.out.println("5");
+	}
 }
